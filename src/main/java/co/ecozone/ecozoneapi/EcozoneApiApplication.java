aeecd0c55aae6f5f727c99b5358b8d131501ec44
Jung <102217402+al1kite@users.noreply.github.com>
2025-08-11 21:34:33 +0900

    chore: initialize Spring Boot project
    - Spring Initializr를 사용해 기본 프로젝트 구조 생성
    - Gradle 기반 빌드 설정
    - Java 23, Spring Boot 3.5.3 적용

---
+++ src/main/java/co/ecozone/ecozoneapi/EcozoneApiApplication.java
@@ -0,0 +1,12 @@
+package co.ecozone.ecozoneapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcozoneApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcozoneApiApplication.class, args);
+    }
+}
