4ed56f3add0008dc864e2d782509953e46f198b6
sojeong <spah7839@gmail.com>
2025-08-31 01:51:14 +0900

    feat(inquiry): InquiryNotFoundException 작성 — 문의 미존재 처리용

---
+++ src/main/java/co/ecozone/ecozoneapi/inquiry/application/exception/InquiryNotFoundException.java
@@ -0,0 +1,5 @@
+package co.ecozone.ecozoneapi.inquiry.application.exception;
+
+public class InquiryNotFoundException extends RuntimeException {
+    public InquiryNotFoundException(String msg) { super(msg); }
+}
